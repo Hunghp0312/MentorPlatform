9b9dad91e9238e3db6eca98de37cbc02ef2a4578
Nguyễn Minh Đức <boydbvn@gmail.com>
2025-05-13 16:17:12 +0700

    Revert "Update Category Entities"
    This reverts commit d4065f7b9428117f0639924b2b1f197ad4d2397e.

--- back-end/ApplicationCore/Entity/Category.cs
+++ back-end/ApplicationCore/Entity/Category.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ApplicationCore.Entity
 {
     public class Category
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(1000)]
         public string Description { get; set; }
+        [Required]
         public bool Status { get; set; }
-        public ICollection<Course> Courses { get; set; }
-        public int CourseCount { get; set; }
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+
+
+        //not apear in database
+        public int CourseCount => Courses?.Count ?? 0;
     }
 }
