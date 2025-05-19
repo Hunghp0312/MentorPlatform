9bdf9da36df2f2ba7f8c8f91415d770a63ac8cef
Lê Anh Quang <142723261+LAQ27504@users.noreply.github.com>
2025-05-19 17:57:44 +0700

    rename file

--- back-end/Infrastructure/Data/Configuration/LookupTypes/CourseLevelConfiguration.cs
+++ back-end/Infrastructure/Data/Configuration/LookupTypes/CourseLevelConfiguration.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configuration.LookupTypes
 {
-    public class CourseLEvelConfiguration : BaseTypeConfiguration<CourseLevel>
+    public class CourseLevelConfiguration : BaseTypeConfiguration<CourseLevel>
     {
         public override void Configure(EntityTypeBuilder<CourseLevel> builder)
         {
