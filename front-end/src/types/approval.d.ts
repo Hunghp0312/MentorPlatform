eb7856608fd055f45e4fe627248528f65c0ec802
Nguyễn Minh Đức <boydbvn@gmail.com>
2025-05-22 22:18:07 +0700

    update entity

--- front-end/src/types/approval.d.ts
+++ front-end/src/types/approval.d.ts
@@ -1,26 +1,16 @@
+import { EnumType } from "./commonType";
+
 // src/types/approval.ts
 export interface ApprovalType {
   id: string | number;
-  name: string;
-  email: string;
-  expertiseAreas: string[]; // For detailed view
-  status: "pending" | "approved" | "rejected"; // 1 = Pending, 2 = Approved
-  submittedDate: string;
-  profileImage: string;
-  experience: string;
-  documents: { name: string; type: "PDF" | "JPG"; url: string }[];
-  adminNotes?: string;
+  // user: undefined;
+  status: EnumType; // 1 = Pending, 2 = Approved 3= Rejected
+  motivationStatement: string;
+  submissionDate: string;
+  lastStatusUpdateDate?: string;
+  // admin?: undefined;
+  adminComment?: string;
+  rejectReason?: string;
+  createAt?: string;
+  approveDate?: string;
 }
-
-// export interface ApprovalCreateType {
-//   name: string;
-//   email: string;
-//   profileImage: string;
-//   //skills: string;
-//   expertiseAreas: string[];
-//   professionalExperience: string;
-//   status: "pending" | "approved" | "rejected";
-//   submittedDate: string;
-//   documents: { name: string; type: "PDF" | "JPG"; url: string }[];
-//   adminNotes?: string;
-// }
