6a9bdb0e77f0e4833901acf4dd88484456d4b893
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-05-27 20:12:08 +0700

    Delete file, uploadFile finish

--- back-end/ApplicationCore/Services/MentorService.cs
+++ back-end/ApplicationCore/Services/MentorService.cs
@@ -233,7 +233,7 @@ namespace ApplicationCore.Services
         }
 
         public async Task<OperationResult<MentorApplicationResponseDto>> UpdateMyApplicationAsync(
-         UpdateMyApplicationApiRequest apiRequest, Guid applicantUserId, bool containFile)
+         UpdateMyApplicationApiRequest apiRequest, Guid applicantUserId)
         {
             var existingApplication = await _mentorRepository.GetByIdAsync(applicantUserId);
             if (existingApplication == null)
@@ -272,7 +272,7 @@ namespace ApplicationCore.Services
                 }
                 existingApplication.LastStatusUpdateDate = DateTime.UtcNow;
                 existingApplication.UpdatedAt = DateTime.UtcNow;
-                existingApplication.ApplicationStatusId = containFile ? 4 : 1;
+                existingApplication.ApplicationStatusId = 1;
                 existingApplication.SubmissionDate += $", {DateTime.UtcNow}";
 
                 _mentorRepository.Update(existingApplication);
