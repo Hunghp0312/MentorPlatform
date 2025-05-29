0efb8b55627d94c4f892445d50007f28219ffdeb
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-05-29 22:43:14 +0700

    Change to 1 minute

--- back-end/ApplicationCore/Services/TokenService.cs
+++ back-end/ApplicationCore/Services/TokenService.cs
@@ -25,7 +25,7 @@ namespace ApplicationCore.Services
                 issuer: issuer,
                 audience: audience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(50),
+                expires: DateTime.UtcNow.AddMinutes(1),
                 signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
                     SecurityAlgorithms.HmacSha256)
             );
