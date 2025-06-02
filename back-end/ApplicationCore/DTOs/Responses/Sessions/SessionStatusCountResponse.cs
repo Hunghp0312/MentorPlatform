d6c9a5c16a287f5344166e10efbfa82a93648009
Nguyễn Minh Đức <boydbvn@gmail.com>
2025-06-03 02:29:51 +0700

    update session status update service and add new count and getall session services which return list of session that are past Session and upcomming session

---
+++ back-end/ApplicationCore/DTOs/Responses/Sessions/SessionStatusCountResponse.cs
@@ -0,0 +1,8 @@
+namespace ApplicationCore.DTOs.Responses.Sessions
+{
+    public class SessionStatusCountResponse
+    {
+        public int UpcommingSessionCount { get; set; }
+        public int PastSessionCount { get; set; }
+    }
+}
