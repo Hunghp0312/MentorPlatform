7aae7884405ec7a9440b14ed7ad2ca9051996a2f
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-06-04 08:15:20 +0700

    Modify schedule-by-day info

--- back-end/ApplicationCore/DTOs/Responses/Mentors/MentorDayDto.cs
+++ back-end/ApplicationCore/DTOs/Responses/Mentors/MentorDayDto.cs
@@ -3,6 +3,10 @@
     public class MentorDayDto
     {
         public Guid Id { get; set; }
+        public TimeOnly StartWorkTime { get; set; }
+        public TimeOnly EndWorkTime { get; set; }
+        public List<string> ExpertiseTags { get; set; } = new List<string>();
+        public string MentorFullName { get; set; } = string.Empty;
         public DateOnly Date { get; set; }
         public List<MentorTimeSlotDto> MentorTimeSlots { get; set; } = new List<MentorTimeSlotDto>();
     }
