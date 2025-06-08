09a25ca70ab0c59bb7c20133e4c09acd9afdb116
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-06-08 15:33:19 +0700

    Set maxlength to cancelReason

--- back-end/ApplicationCore/Validators/SessionBookings/UpdateBookingStatusRequestDtoValidator.cs
+++ back-end/ApplicationCore/Validators/SessionBookings/UpdateBookingStatusRequestDtoValidator.cs
@@ -11,6 +11,12 @@ namespace ApplicationCore.Validators.SessionBookings
             RuleFor(x => x.NewStatusId)
               .InclusiveBetween(1, 6)
               .WithMessage(ValidationMessages.SessionStatusIdInvalidRange);
+
+            When(x => x.CancelReason != null, () =>
+            {
+                RuleFor(x => x.CancelReason)
+                    .MaximumLength(1000).WithMessage(ValidationMessages.MaxLengthExceeded);
+            });
         }
     }
 }
