3ca2f31ddd5543b050cc44b20a08f39d147ab133
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-06-11 12:57:27 +0700

    StudentEnrollments

--- back-end/ApplicationCore/DTOs/Responses/Courses/CourseDashboardKpiDto.cs
+++ back-end/ApplicationCore/DTOs/Responses/Courses/CourseDashboardKpiDto.cs
@@ -3,6 +3,7 @@
     public class CourseDashboardKpiDto
     {
         public int TotalCourses { get; set; }
+        public int StudentEnrollments { get; set; }
         public int AverageCompletion { get; set; }
         public int PublishedCourses { get; set; }
     }
