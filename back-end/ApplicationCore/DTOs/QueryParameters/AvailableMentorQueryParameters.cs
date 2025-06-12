b144265355b57d08a2c0eeef89a31320353f71be
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-06-13 05:46:16 +0700

    Seed Expertise

--- back-end/ApplicationCore/DTOs/QueryParameters/AvailableMentorQueryParameters.cs
+++ back-end/ApplicationCore/DTOs/QueryParameters/AvailableMentorQueryParameters.cs
@@ -5,6 +5,6 @@ namespace ApplicationCore.DTOs.QueryParameters
     public class AvailableMentorQueryParameters : PaginationParameters
     {
         public int TopicId { get; set; }
-        public List<int> ExpertiseIds { get; set; } = new List<int>();
+        public List<int> ExpertiseIds { get; set; } = new List<int>() { 1 };
     }
 }
